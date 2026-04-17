3b6cfda446c978ffc8ee3af2c8ce357f858fe113
double-luck88 <3321915966@qq.com>
2026-04-17 12:41:18 +0800

    Add files via upload

---
+++ 窗口创建.py
@@ -0,0 +1,27 @@
+import pygame as pg
+
+
+#初始化pygame
+pg.init()
+
+#创建一个窗口,宽度800，高度600
+window=pg.display.set_mode((800,600))
+
+#设置窗口标题
+pg.display.set_caption("hello pygame!")
+
+#设置窗口背景颜色,RGE
+#window.fill((217,217,217))
+#window.fill('red')
+window.fill(pg.Color(227,227,227))
+
+
+#更新窗口
+pg.display.update()
+
+#暂停一会窗口,5000=5s
+pg.time.wait(5000)
+
+
+#清理，退出pygame
+pg.quit()
